a0fa6000f9cb9976d6ad2a4574b07e969db19d97
brian tarigan <2381051@unai.edu>
2024-10-03 22:05:31 +0700

    handson5

--- MainJava.java
+++ MainJava.java
@@ -5,13 +5,7 @@ public class MainJava {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        addTodoList("Menulis");
-        addTodoList("Menggambar");
-        addTodoList("Membaca");
-        showTodolist();
-        removeTodolist(2);
-        System.out.println("after delete");
-        showTodolist();
+        showMainMenu();
     }
 
     public static void showTodolist() {
@@ -27,8 +21,8 @@ public class MainJava {
     public static void addTodoList(String todo) {
         resizeArrayIfFull();
 
-        for(int i = 0; i < todos.length; i ++){
-            if ( todos[i] == null){
+        for (int i = 0; i < todos.length; i++) {
+            if (todos[i] == null) {
                 todos[i] = todo;
                 break;
             }
@@ -92,29 +86,95 @@ public class MainJava {
         return false;
     }
 
+    public static boolean editTodoList(Integer number, String newTodo) {
+        if (isSelectedTodoNotValid(number)) {
+            return false;
+        }
+        todos[number -1] = newTodo;
+        return true;
+    }
+
     public static void showMainMenu() {
         boolean isRunning = true;
-        while ( isRunning) {
-            showTodolist();;
+        while (isRunning) {
+            showTodolist();
+            ;
             System.out.println("menu");
             System.out.println("1. Tambah");
             System.out.println("2. Hapus");
             System.out.println("3. Edit");
             System.out.println("4. Keluar");
-            String selectedMenu = scanner.nextLine();
+            String selectedMenu = input("pilih");
             switch (selectedMenu) {
                 case "1":
-                break;
+                    showMenuAddTodoList();
+                    System.out.println("Menu add todo list");
+                    break;
                 case "2":
-                break;
+                    showMenuRemoveTodoList();
+                    System.out.println("Menu remove todo list");
+                    break;
                 case "3":
-                break;
+                    showMenuEditTodoList();
+                    System.out.println("Menu  edit todo list");
+                    break;
                 case "4":
                     isRunning = false;
                     break;
                 default:
+                    System.out.println("Pilih menu dengan benar");
             }
 
         }
     }
+
+    public static String input(String info) {
+        System.out.print(info + " : ");
+        String data = scanner.nextLine();
+        return data;
+    }
+
+
+    public static void showMenuAddTodoList() {
+        System.out.println("MENAMBAH TODO LIST");
+        String todo = input("todo (x jika batal)");
+        if (todo.equals("x")) {
+            //batal
+        } else {
+            addTodoList(todo);
+        }
+    }
+
+    public static void showMenuRemoveTodoList() {
+        System.out.println("MENGHAPUS TODO LIST");
+        String todoYangDipilih = input("Nomor todo yang dihapus (x jika batal)");
+        if (todoYangDipilih.equals("x")) {
+            //batal
+        } else {
+            boolean success = removeTodolist(Integer.valueOf(todoYangDipilih));
+            if (!success) {
+                System.out.println("Gagal menghapus todo list : " + todoYangDipilih);
+            }
+        }
+    }
+
+    public static void showMenuEditTodoList() {
+        System.out.println("MENGEDIT TODO LIST");
+        String selectedTodo = input("Masukkan nomor todo (x jika batal)");
+        if (selectedTodo.equals("x")) {
+            return;
+        }
+        String newTodo = input("Masukkan todo yang baru (x jika batal)");
+        if (newTodo.equals("x")) {
+            return;
+        }
+        boolean isEditTodoSuccess = editTodoList(Integer.valueOf(selectedTodo), newTodo);
+        if (isEditTodoSuccess) {
+            System.out.println("Berhasil mengedit todo");
+        } else {
+            System.out.println("Gagal mengedit todo");
+        }
+    }
 }
+
+
